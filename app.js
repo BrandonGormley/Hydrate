bf3872712b531a4664d51f3940d3b85f1152d5b6
brandontgormley@gmail.com <brandontgormley@gmail.com>
2020-05-10 19:19:29 -0400

    Fav Icon / Title
    -Updated Fav Icon / Title

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ let button = document.querySelector('.water-button');
 let waterIntake = document.querySelector('#water-intake');
 
 button.addEventListener('click', () => {
-    waterIntake.textContent = (parseInt(weight.value) * .67) + ' fluid ounces daily.';
+    waterIntake.textContent = parseInt(weight.value) * .67 + ' fluid ounces daily.';
 });
