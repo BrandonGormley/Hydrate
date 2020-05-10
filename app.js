f89eae0c38a708df77e3ceab0f7c65b037b84280
brandontgormley@gmail.com <brandontgormley@gmail.com>
2020-05-10 12:10:26 -0400

    Input Bug
    -Fixed a bug where if you input your weight with lbs at the end it returned NaN.
    -It now returns correct number.

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ let button = document.querySelector('.water-button');
 let waterIntake = document.querySelector('#water-intake');
 
 button.addEventListener('click', () => {
-    waterIntake.textContent = (weight.value * .67) + ' fluid ounces';
+    waterIntake.textContent = (parseInt(weight.value) * .67) + ' fluid ounces daily.';
 });
